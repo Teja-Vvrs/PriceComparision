e4165708e5b6399ce3bd55a16d08b4289ae868d5
Teja-Vvrs <tejav2527@gmail.com>
2025-04-09 21:29:31 +0530

    add NotFound component for handling 404 errors

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold underline text-slate-500'>
+            404 Not Found
+        </h1>
+        <p className='color-red'>The page you are looking for does not exist.</p>
+
+    </div>
+  )
+}
+
+export default NotFound
