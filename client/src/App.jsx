8f4e006c077f164fbe716bed17657762b21e140c
Teja-Vvrs <tejav2527@gmail.com>
2025-05-02 14:13:41 +0530

    started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline text-slate-500">
-           The Price Comparison App
-      </h1>
-      <p className='color-green'>this is for the customers who are used to comapre ecommerce electronics in amazon</p>
+      
+
+
+
     </div>
   )
 }
